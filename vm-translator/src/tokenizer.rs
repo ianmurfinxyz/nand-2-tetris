9086b6a54debb2da6770af1d603ce635fda414cf
ianmurfinxyz <ianmurfin@protonmail.com>
2023-04-12 10:38:36 +0100

    Reworked the read logic as the borrow checker says nope
    ...cannot move out of a &mut Self, as this would allow us to consume the Self, however &mut is a guarantee that the Self can be mutated, but not consumed. All this means in practice is that all members of self must remain initialised, we can still mutate the buf and str to a length of 0 (even a capacity of 0), but they must be initialised. Hence to overcome the &mut self I am swapping out of the self buffers into local buffers, and then swapping back. This ensures all self members remain initialised.

--- vm-translator/src/tokenizer.rs
+++ vm-translator/src/tokenizer.rs
@@ -1,6 +1,7 @@
 use compact_str::CompactString;
 use std::io::{self, BufRead};
 use std::string::FromUtf8Error;
+use std::mem::swap;
 
 pub enum VmCmd {
 	Function,
@@ -72,15 +73,15 @@ const MAX_READ_SIZE_BYTES: usize = 4096;
 
 pub struct Tokenizer<R: BufRead> {
 	reader: R,
-	buffer: Vec<u8>,
-	view: String,
+	buf: Vec<u8>,
+	str: String,
 	pos: usize,
 	end: usize,
 }
 
 impl<R: BufRead> Tokenizer<R> {
 	fn new(reader: R) -> Self {
-		Tokenizer{reader, buffer: Vec::with_capacity(MAX_READ_SIZE_BYTES), view: String::new(), pos: 0, end: 0}
+		Tokenizer{reader, buf: Vec::with_capacity(MAX_READ_SIZE_BYTES), str: String::new(), pos: 0, end: 0}
 	}
 }
 
@@ -91,9 +92,9 @@ impl<R: BufRead> Iterator for Tokenizer<R> {
 
 		// Test if the entire next token is currently in the buffer.
 		let mut next_token_end_pos = None;
-		for (i, c) in self.view[self.pos..].chars().enumerate() {
+		for (pos, c) in self.str[self.pos..].chars().enumerate() {
 			if c.is_whitespace() {
-				next_token_end_pos = Some(i);
+				next_token_end_pos = Some(pos);
 				break;
 			}
 		};
@@ -101,24 +102,41 @@ impl<R: BufRead> Iterator for Tokenizer<R> {
 		// If next token is not in the buffer we need to repopulate the buffer.
 		if next_token_end_pos.is_none() {
 
+			// The borrow checker will not allow us to uninitialise the buf and str
+			// from a &mut self, so to transfer ownership from the buf to the str
+			// and visa versa we need to ensure the self.buf and self.str remain
+			// initialised, which we can do by swapping in empty versions.
+			let mut buf = Vec::new();
+			let mut str = String::new();
+			swap(&mut self.buf, &mut buf);
+			swap(&mut self.str, &mut str);
+
+			// We are trying to transfer ownership between these buffers...make sure
+			// we havn't accidentally deallocated our memory.
+			debug_assert!(buf.capacity() > 0 || str.capacity() > 0);
+
 			// Transfer ownership of the buffer back to the vec.
-			if !self.view.is_empty() {
-				self.buffer = self.view.into_bytes();
-				self.view.clear();
+			if str.capacity() > 0 {
+				debug_assert!(buf.capacity() == 0);
+				buf = str.into_bytes();
 			}
+
+			debug_assert!(buf.capacity() > 0);
 
 			// Bytes at the end of the buffer we have not yet processed.
 			let remaining_bytes_count = self.end - self.pos;
 
 			// If the next token is partially in the buffer, shift it to the start.
 			if remaining_bytes_count > 0 {
-				self.buffer[0..].copy_from_slice(&self.buffer[self.pos..]);
-				self.end = remaining_bytes_count;
-				self.pos == 0;
+				let (left, right) = buf.split_at_mut(self.pos);
+				left.copy_from_slice(&right);
 			}
 
+			self.end = remaining_bytes_count;
+			self.pos = 0;
+
 			loop {
-				match self.reader.read(&mut self.buffer[self.end..]) {
+				match self.reader.read(&mut buf[self.end..]) {
 					Ok(n) => { self.end += n; break; },
 					Err(ref e) if e.kind() == std::io::ErrorKind::Interrupted => continue,
 					Err(e) => return Some(Err(TokenError::from(e))),
@@ -128,16 +146,18 @@ impl<R: BufRead> Iterator for Tokenizer<R> {
 			// We do self.end += n...safety first!
 			debug_assert!(self.end <= MAX_READ_SIZE_BYTES);
 
+			// Will transfer ownership from the buffer to the string.
+			str = match String::from_utf8(buf) {
+				Ok(s) => s,
+				Err(e) => return Some(Err(TokenError::from(e))),
+			};
+
+			swap(&mut self.str, &mut str);
+
 			// If we hit EOF...
 			if self.end == 0 {
 				return None
 			}
-
-			// Will transfer ownership from the buffer to the string.
-			self.view = match String::from_utf8(self.buffer) {
-				Ok(s) => s,
-				Err(e) => return Some(Err(TokenError::from(e))),
-			};
 		}
 
 
